8ff839e3f2bb3ed2e53bfc96b025b304e9156f7d
wazir <wazir.murad02@gmail.com>
2024-10-06 16:36:19 +0200

    Ergänzung der translateNumber Methode mithilfe der Datenstruktur Arraylist.

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
@@ -1,16 +1,37 @@
 package org.hbrs.se1.ws24.exercises.uebung1.control;
 
+import java.util.ArrayList;
+
 public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2024"; // Default-Wert
 
+	static ArrayList<String> list = new ArrayList<>();
+	static {
+		list.add("eins");
+		list.add("zwei");
+		list.add("drei");
+		list.add("vier");
+		list.add("fünf");
+		list.add("sechs");
+		list.add("sieben");
+		list.add("acht");
+		list.add("neun");
+		list.add("zehn");
+	}
+
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
-	public String translateNumber( int number ) {
-		// [ihr Source Code aus Übung 1-2]
-
-		return "null";
+	@Override
+	public String translateNumber(int number) {
+		try {
+			// Der Index muss um 1 angepasst werden, da die Liste nullbasiert ist
+			return list.get(number - 1);
+		} catch (IndexOutOfBoundsException e) {
+			// Wenn die Zahl außerhalb des Bereichs 1-10 liegt, wird die Exception ausgelöst
+			return "Übersetzung der Zahl " + number + " nicht möglich (Version " + Translator.version + ")";
+		}
 	}
 
 	/**
