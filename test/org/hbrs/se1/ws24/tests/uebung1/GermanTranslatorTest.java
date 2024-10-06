3234e1ba9906415127d993c6e7f8dc61ee91cd74
wazir <wazir.murad02@gmail.com>
2024-10-06 16:38:29 +0200

    Durchführung von positv- und negativ-Testfällen.

--- test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
@@ -1,15 +1,24 @@
 package org.hbrs.se1.ws24.tests.uebung1;
 import static org.junit.jupiter.api.Assertions.*;
 import org.hbrs.se1.ws24.exercises.uebung1.control.GermanTranslator;
+import org.hbrs.se1.ws24.exercises.uebung1.control.Translator;
 import org.junit.jupiter.api.Test;
 
 public class GermanTranslatorTest {
 
     @Test
-    public void aTest() {
-        GermanTranslator translator = new GermanTranslator();
+    void aTest() {
+        Translator translator = new GermanTranslator();
+        assertEquals("eins", translator.translateNumber(1));
+        assertEquals("zwei", translator.translateNumber(2));
+        assertEquals("zehn", translator.translateNumber(10));
 
-        assertEquals("null" , translator.translateNumber(0));
+    }
+    @Test
+    void bTest() {
+        Translator translator = new GermanTranslator();
+        assertEquals("Übersetzung der Zahl 0 nicht möglich (Version 1.0)", translator.translateNumber(0));
+        assertEquals("Übersetzung der Zahl -9 nicht möglich (Version 1.0)", translator.translateNumber(-9));
     }
 
 }
