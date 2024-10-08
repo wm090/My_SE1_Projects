1a93d29f5bc9f4591534922c0de3e45d24d57933
wazir <wazir.murad02@gmail.com>
2024-10-08 22:35:26 +0200

    Bereitstellung einer Klasse ConcreteMember, die das Interface-Member implementiert.

--- src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
@@ -1,5 +1,10 @@
 package org.hbrs.se1.ws24.exercises.uebung2;
 
-public class ConcreteMember {
+public class ConcreteMember implements Member {
 
+    @Override
+    public Integer getID() {
+
+        return 0;
+    }
 }
