16a5fdf3ed2f8e1d8932c64407572008227fd47d
wazir <wazir.murad02@gmail.com>
2024-10-11 18:27:25 +0200

    Erzeugung eines Integer Objekts sowie Übergabe dieses über den Konstruktor

--- src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
@@ -2,9 +2,20 @@ package org.hbrs.se1.ws24.exercises.uebung2;
 
 public class ConcreteMember implements Member {
 
+    private Integer id;
+
+    public ConcreteMember(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public Integer getID() {
+        return this.id;
+    }
 
-        return 0;
+    //FA3: To-String Methode
+    @Override
+    public String toString() {
+        return "Member (ID = " + this.id + ")";
     }
 }
