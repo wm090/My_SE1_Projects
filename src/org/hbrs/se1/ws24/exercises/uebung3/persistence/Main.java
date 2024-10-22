bb84a0a9375e00de0c59e1d6e2266180c014645d
wazir <wazir.murad02@gmail.com>
2024-10-22 17:20:35 +0200

    U3_CR3: - Dient zur Erzeugung eines Client Objektes, um die Mehtode ausgebe() aus client aufzurufen.

---
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/Main.java
@@ -0,0 +1,13 @@
+package org.hbrs.se1.ws24.exercises.uebung3.persistence;
+
+import org.hbrs.se1.ws24.exercises.uebung2.ContainerException;
+
+public class Main {
+
+    //Übung 3 - CR3
+    public static void main(String[] args) throws ContainerException {
+        Client c = new Client();
+        c.ausgabe();
+
+    }
+}
