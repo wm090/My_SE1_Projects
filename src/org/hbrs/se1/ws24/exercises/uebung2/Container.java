6ad10e83d240d1b1c93b8a6d9727365d2d3b9e48
wazir <wazir.murad02@gmail.com>
2024-10-22 17:10:50 +0200

    - Deklaration eines Container Objekts sowie Intialisierung dieses Objekts im Konstruktor als ArrayList. - Unter Verwendung des Singleton Design Pattern ist es möglich nur ein einziges Objekt von der Klasse Container zu erzeugen.

--- src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
@@ -1,16 +1,61 @@
 package org.hbrs.se1.ws24.exercises.uebung2;
 
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.MemberView;
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceStrategy;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Container {
 
+    //Deklaration eines privaten und statischen ContainerVariable
+    private static Container instance;
+
     // Liste zur Speicherung von Member-Objekten.
     private List<Member> memberContainer;
 
-    public Container() {
+    //Konstruktor Zugriffsrecht auf private gestellt
+    private Container() {
         memberContainer = new ArrayList<>();
     }
+
+    //Übung 3-----------------------------------------------------------------------------
+    //CR1 - Singleton Design Pattern
+    public static Container getInstance() {
+        if (instance == null) {
+            instance = new Container();
+        }
+        return instance;
+    }
+
+    //CR2
+    private PersistenceStrategy<Member> persistenceStrategy;
+
+    public void setPersistenceStrategy(PersistenceStrategy<Member> strategy) {
+        persistenceStrategy = strategy;
+    }
+    public void store() throws PersistenceException {
+        if (persistenceStrategy == null){
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet, "No Strategy Set");
+        }
+        persistenceStrategy.save(memberContainer);
+    }
+
+    public void load() throws PersistenceException {
+        if (persistenceStrategy == null){
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet, "No Strategy Set");
+        }
+        List<Member> members = persistenceStrategy.load();
+        memberContainer = members != null ? members : new ArrayList<>();
+    }
+
+    //CR3
+    public List<Member> getCurrentList() {
+        return new ArrayList<>(memberContainer);
+    }
+
+    //Übung 2-Ende-------------------------------------------------------------------------
 
     //Main zur Kontrolle
     public static void main(String[] args) {
@@ -19,7 +64,12 @@ public class Container {
         Member m3 = new ConcreteMember(3);
         Member m4 = new ConcreteMember(1);
 
-        Container c = new Container();
+       Container c = new Container(); //Funktioniert nur, weil innerhalb der Klasse.
+       Container c2 = new Container(); //Enthält nicht das gleiche wie c.
+
+        //Singleton Design Pattern
+        Container c3 = Container.getInstance();
+        Container c4 = Container.getInstance(); //Enthält genau das gleiche wie c3, da nur ein Objekt
 
         try {
             c.addMember(m1);
@@ -30,13 +80,17 @@ public class Container {
             System.out.println(e.getMessage());
         }
 
-
         //System.out.println(c.deleteMember(4));
 
         c.dump();
         System.out.println(c.size());
+        System.out.println("C2");
+        c2.dump();
+        System.out.println("C2");
+
     }
 
+    //Übung 2-------------------------------------------------------------------------------------
     //FA1:
     public void addMember(Member member) throws ContainerException {
         for (Member m : memberContainer) {
@@ -77,5 +131,6 @@ public class Container {
     public int size() {
         return memberContainer.size();
     }
+    //Übung 2-Ende-------------------------------------------------------------------------------
 
 }
